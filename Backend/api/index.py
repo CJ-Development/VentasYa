010a974fa23d4d87c9e25631555c423af8d0b405
Jhon <jhoncortes876@gmail.com>
2026-08-20 10:40:14 -0500

    fix: configurar Django para Vercel

--- Backend/api/index.py
+++ Backend/api/index.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-# Agrega la raíz del backend al PATH
+# Ruta raíz del backend
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
 # Configuración de Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "confiig.settings")
 
 from django.core.wsgi import get_wsgi_application
 
