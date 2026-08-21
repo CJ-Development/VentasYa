85faef300c9103e2a16e0590e147a7f9f65339f6
Jhon <jhoncortes876@gmail.com>
2026-08-21 01:19:47 -0500

    Fix endpoint CSRF

--- Backend/apps/users/urls.py
+++ Backend/apps/users/urls.py
@@ -10,7 +10,7 @@ from .views import (
     CambiarPasswordView,
     DireccionView,
     DireccionDetalleView,
-    CsrfView,
+    CsrfTokenView,
 )
 
 urlpatterns = [
@@ -18,14 +18,26 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("me/", MeView.as_view(), name="user-me"),
-    path("csrf/", CsrfView.as_view(), name="csrf"),
+    path("csrf/", CsrfTokenView.as_view(), name="csrf"),
+
     path("", UserListView.as_view(), name="users"),
     path("<int:id>/", UsuarioDetalleView.as_view(), name="user-detail"),
-    path("<int:id>/cambiar-password/", CambiarPasswordView.as_view(), name="user-change-password"),
+    path(
+        "<int:id>/cambiar-password/",
+        CambiarPasswordView.as_view(),
+        name="user-change-password"
+    ),
 
-    path("direcciones/", DireccionView.as_view(), name="user-direcciones"),
-    path("direcciones/<int:id>/", DireccionDetalleView.as_view(), name="user-direccion-detalle"),
-    
+    path(
+        "direcciones/",
+        DireccionView.as_view(),
+        name="user-direcciones"
+    ),
+    path(
+        "direcciones/<int:id>/",
+        DireccionDetalleView.as_view(),
+        name="user-direccion-detalle"
+    ),
 ]
 
 app_name = "users"
